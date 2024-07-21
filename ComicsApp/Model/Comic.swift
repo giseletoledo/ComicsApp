841203c6a26eb217449fa67a8d102f10eae521ca
gisele <gisesonia@gmail.com>
2024-07-21 00:02:54 -0300

    feat: model de comics

--- ComicsApp/Model/Comic.swift
+++ ComicsApp/Model/Comic.swift
@@ -7,58 +7,24 @@
 
 import Foundation
 
+struct ComicDataWrapper: Codable {
+    let data: ComicData
+}
+
+struct ComicData: Codable {
+    let results: [Comic]
+}
+
 struct Comic: Codable {
     let id: Int
     let title: String
+    let description: String?
     let isbn: String?
     let thumbnail: Thumbnail?
-    
-    struct Thumbnail: Codable {
-        let path: String
-        let `extension`: String
-    }
 }
 
-class MarvelAPIService {
-    private let baseURL = "https://gateway.marvel.com:443/v1/public/comics"
-    private let apiKey = "YOUR_API_KEY"
-    private let privateKey = "YOUR_PRIVATE_KEY"
-    
-    func fetchAllComics(completion: @escaping ([Comic]?, Error?) -> Void) {
-        let urlString = "\(baseURL)?apikey=\(apiKey)"
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data"]))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let response = try decoder.decode(ComicDataWrapper.self, from: data)
-                completion(response.data.results, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }
-        task.resume()
-    }
-}
-
-struct ComicDataWrapper: Codable {
-    let data: ComicDataContainer
-}
-
-struct ComicDataContainer: Codable {
-    let results: [Comic]
+struct Thumbnail: Codable {
+    let path: String
+    let `extension`: String
 }
 
