5391e543320d24721dc13f84b8df0c7cf1101513
gisele <gisesonia@gmail.com>
2024-07-21 00:03:46 -0300

    feat: extension para carregar imagens da API

--- ComicsApp/UIImageView+Extensions.swift
+++ ComicsApp/UIImageView+Extensions.swift
@@ -5,4 +5,17 @@
 //  Created by GISELE TOLEDO on 20/07/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }
+    }
+}
+
