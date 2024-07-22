d5d105e1de30104463f9c2213ac76c7475d1e7f1
gisele <gisesonia@gmail.com>
2024-07-22 14:34:32 -0300

    refactor: carregando imagem com extension

--- ComicsApp/ViewControllers/ViewController.swift
+++ ComicsApp/ViewControllers/ViewController.swift
@@ -73,6 +73,7 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return comics.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell") else {
             print("Failed to dequeue cell")
@@ -85,14 +86,15 @@ extension ViewController: UITableViewDataSource {
         var contentConfig = cell.defaultContentConfiguration()
         contentConfig.text = comic.title
         contentConfig.secondaryText = comic.description
-
+        
         // Define o URL padrão para imagem não disponível
         let placeholderURL = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
         
         if let thumbnail = comic.thumbnail {
             let urlString = "\(thumbnail.path).\(thumbnail.extension)"
             let url = URL(string: urlString) ?? URL(string: placeholderURL)!
-            loadImage(from: url) { image in
+            let imageView = UIImageView()
+            imageView.loadImage(from: url) { image in
                 DispatchQueue.main.async {
                     var updatedContentConfig = cell.defaultContentConfiguration()
                     updatedContentConfig.text = comic.title
@@ -109,22 +111,10 @@ extension ViewController: UITableViewDataSource {
             contentConfig.imageProperties.reservedLayoutSize = CGSize(width: 55, height: 75)
             cell.contentConfiguration = contentConfig
         }
-
+        
         print("Configured cell for row \(indexPath.row)")
         return cell
     }
-
-    func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil, let image = UIImage(data: data) else {
-                completion(nil)
-                return
-            }
-            completion(image)
-        }
-        task.resume()
-    }
-
 }
 
 extension ViewController {
